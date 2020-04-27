d6b34cb40d6fe459d05ef428bdfd8637122a255b
druskus20 <druskus20@gmail.com>
2020-04-27 19:02:58 +0200

    removed stupid shit from polybar yay script, also fixed it

--- .config/polybar/modules/updates-arch-aur.sh
+++ .config/polybar/modules/updates-arch-aur.sh
@@ -1,15 +1,4 @@
-#!/bin/sh
+# !/bin/sh
 
-if ! updates=$(yay -Qum 2> /dev/null | wc -l); then
-# if ! updates=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates=$(trizen -Su --aur --quiet | wc -l); then
-# if ! updates=$(pikaur -Qua 2> /dev/null | wc -l); then
-# if ! updates=$(rua upgrade --printonly 2> /dev/null | wc -l); then
-    updates=0
-fi
-
-if [ "$updates" -gt 0 ]; then
-    echo "$updates"
-else
-    echo ""
-fi
+updates=$(yay -Qu 2> /dev/null | wc -l)  || updates=0
+( [ "$updates" -gt 0 ] && echo "$updates" ) || echo "" 
