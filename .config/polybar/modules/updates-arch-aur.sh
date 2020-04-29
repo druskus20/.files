f1e006bf69d1b1ebcfb627829b37c49b182eabe3
druskus20 <druskus20@gmail.com>
2020-04-29 21:00:54 +0200

    polybar ethernet module packetloss

--- .config/polybar/modules/updates-arch-aur.sh
+++ .config/polybar/modules/updates-arch-aur.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
 
 updates=$(yay -Qu 2> /dev/null | wc -l)  || updates=0
-( [ "$updates" -gt 0 ] && echo "$updates" ) || echo "" 
+( [ "$updates" -gt 0 ] && echo "$updates" ) || echo "0" 
